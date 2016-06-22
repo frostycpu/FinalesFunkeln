1bc05de19adb85e1e94353cdc4fa18b4b690869c
Nico Boettcher <frostygpu@gmail.com>
2016-06-22 17:28:38 +0200

    Add some image caching

--- FinalesFunkeln/Lol/LolClientImages.cs
+++ FinalesFunkeln/Lol/LolClientImages.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
@@ -19,6 +20,12 @@ namespace FinalesFunkeln.Lol
         readonly string _clientDir;
         readonly LolClientGameData _gameData;
 
+        readonly Dictionary<int, BitmapImage> iconCache = new Dictionary<int, BitmapImage>();
+        readonly Dictionary<int, BitmapImage> splashCache = new Dictionary<int, BitmapImage>();
+        readonly Dictionary<int, BitmapImage> portraitCache = new Dictionary<int, BitmapImage>();
+        readonly Dictionary<int, BitmapImage> spellIconCache = new Dictionary<int, BitmapImage>();
+        readonly Dictionary<int, BitmapImage> wardSkinCache = new Dictionary<int, BitmapImage>();
+
         internal LolClientImages(string basePath, LolClientGameData gameData)
         {
             if (basePath == null)
@@ -32,18 +39,52 @@ namespace FinalesFunkeln.Lol
             string name = null;
             if (championId != 0)
                 name = _gameData.GetChampionData(championId)?.Name;
-
-            return new BitmapImage(new Uri(Path.Combine(_clientDir, name == null ? RandomIconFile : string.Format(IconFile, name))));
+            BitmapImage bi;
+            if (!iconCache.ContainsKey(championId))
+            {
+                bi = new BitmapImage(new Uri(Path.Combine(_clientDir, name == null ? RandomIconFile : string.Format(IconFile, name))));
+                iconCache[championId] = bi;
+                bi.Freeze();
+            }
+            else
+                bi = iconCache[championId];
+            return bi;
         }
 
         public BitmapImage GetChampionPortrait(int championId, int skinIndex = 0)
         {
-            return new BitmapImage(new Uri(Path.Combine(_clientDir, string.Format(PortraitFile, _gameData.GetChampionData(championId)?.Name, skinIndex))));
+            int id = championId * 1000 + skinIndex;
+            string name = null;
+            if (championId != 0)
+                name = _gameData.GetChampionData(championId)?.Name;
+            BitmapImage bi;
+            if (!portraitCache.ContainsKey(id))
+            {
+                bi = new BitmapImage(new Uri(Path.Combine(_clientDir, string.Format(PortraitFile, name, skinIndex))));
+                portraitCache[id] = bi;
+                bi.Freeze();
+            }
+            else
+                bi = portraitCache[id];
+            return bi;
         }
 
         public BitmapImage GetChampionSplash(int championId, int skinIndex = 0)
         {
-            return new BitmapImage(new Uri(Path.Combine(_clientDir, string.Format(SplashFile, _gameData.GetChampionData(championId)?.Name, skinIndex))));
+            int id = championId * 1000 + skinIndex;
+            string name = null;
+            if (championId != 0)
+                name = _gameData.GetChampionData(championId)?.Name;
+            BitmapImage bi;
+            if (!splashCache.ContainsKey(id))
+            {
+                bi = new BitmapImage(new Uri(Path.Combine(_clientDir, string.Format(PortraitFile, name, skinIndex))));
+                splashCache[id] = bi;
+                bi.Freeze();
+            }
+            else
+                bi = splashCache[id];
+            return bi;
         }
 
         public BitmapImage GetSummonerSpellIcon(int spellId)
@@ -51,14 +92,36 @@ namespace FinalesFunkeln.Lol
             string filename = string.Format(SummonerSpellFile, spellId);
             if (!File.Exists(filename))
                 return null;
+
+            BitmapImage bi;
+            if (!spellIconCache.ContainsKey(spellId))
+            {
+                bi = new BitmapImage(new Uri(Path.GetFullPath(filename)));
+                spellIconCache[spellId] = bi;
+                bi.Freeze();
+            }
             else
-                return new BitmapImage(new Uri(Path.GetFullPath(filename)));
+                bi = spellIconCache[spellId];
+            return bi;
         }
 
         public BitmapImage GetWardSkin(int wardId)
         {
             string filename = Path.Combine(_clientDir, string.Format(WardSkinFile, wardId));
-            return !File.Exists(filename) ? null : new BitmapImage(new Uri(Path.GetFullPath(filename)));
+            if (!File.Exists(filename))
+                return null;
+
+            BitmapImage bi;
+            if (!wardSkinCache.ContainsKey(wardId))
+            {
+                bi = new BitmapImage(new Uri(Path.GetFullPath(filename)));
+                wardSkinCache[wardId] = bi;
+                bi.Freeze();
+            }
+            else
+                bi = wardSkinCache[wardId];
+            return bi;
         }
+        
     }
 }
